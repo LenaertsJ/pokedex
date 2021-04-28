d87b24d351d0a6e2ddbfe641488dce992aa36e4d
Julie Lenaerts <julielenaerts@gmail.com>
2021-04-28 10:22:34 +0200

    setup react pokedex app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import FormInput from "./components/form-input/form-input.component";
 
 function App() {
   return (
-    <div className="App">
+    <div className="pokedex-app">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Pokedex</h1>
       </header>
+      <FormInput />
     </div>
   );
 }
